5bca3eb20e8f4164f00b9f056ceae14df7c324c1
Andrés Morales Reyes <sharker92@hotmail.com>
2025-06-13 14:13:45 -0600

    👌 IMPROVE: Update crashing gt shorcut

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,10 +1,10 @@
 return {
-    'tpope/vim-fugitive',
-    enabled = true,
-    config = function ()
-        vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-        vim.keymap.set("n", "gt", "<cmd>diffget //2<CR>")
-        vim.keymap.set("n", "gn", "<cmd>diffget //3<CR>")
-    end
+	"tpope/vim-fugitive",
+	enabled = true,
+	config = function()
+		vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+		vim.keymap.set("n", "gh", "<cmd>diffget //2<CR>")
+		vim.keymap.set("n", "gl", "<cmd>diffget //3<CR>")
+	end,
 }
 
